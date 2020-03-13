1a332c231345ae6a1c39b3e3e335b220a18ca9b8
R. Stanley Hum <r.stanley.hum@stanleyhum.com>
2020-03-12 20:17:45 -0400

    removed tests and testing

--- numpy/setup.py
+++ numpy/setup.py
@@ -7,7 +7,7 @@ def configuration(parent_package='',top_path=None):
     config.add_subpackage('compat')
     config.add_subpackage('core')
     config.add_subpackage('distutils')
-    config.add_subpackage('doc')
+    #config.add_subpackage('doc')
     config.add_subpackage('f2py')
     config.add_subpackage('fft')
     config.add_subpackage('lib')
@@ -16,9 +16,9 @@ def configuration(parent_package='',top_path=None):
     config.add_subpackage('matrixlib')
     config.add_subpackage('polynomial')
     config.add_subpackage('random')
-    config.add_subpackage('testing')
-    config.add_data_dir('doc')
-    config.add_data_dir('tests')
+    #config.add_subpackage('testing')
+    #config.add_data_dir('doc')
+    #config.add_data_dir('tests')
     config.make_config_py() # installs __config__.py
     return config
 
